ecc52d77d48e3eb2ffd86ebbc5a2341263998110
Alen <alechkr@gmail.com>
2020-12-25 00:30:58 +0100

    minor changes

--- script.js
+++
@@ -1,8 +0,0 @@
- document.querySelector('.hamburger-icon').addEventListener('click', () => {
-    let menu = document.querySelector('.mobile-menu');
-    menu.classList.toggle('toggle-nav'); 
- });
-
-
- 
- 
