1302cca1ea32c132ce037ef335fc96549dac75f4
Alen <alechkr@gmail.com>
2020-12-27 14:11:57 +0100

    adjusted canvas settings so that the animation stays in the div

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var particles = [];
-var num_particles = 175;//Change that to your liking
+var num_particles = 200;//Change that to your liking
 
 //Helper function to get a random color - but not too dark
 function GetRandomColor() {
